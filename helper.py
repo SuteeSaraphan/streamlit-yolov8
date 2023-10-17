98fb90feb3a62cf0b5acf137067dac07a63df8dc
Yannawut tin <58814744+somearthzaza@users.noreply.github.com>
2023-10-18 00:37:30 +0700

    fix send api

--- helper.py
+++ helper.py
@@ -318,20 +318,18 @@ def detect(source, list_polygon, conf, model):
         else:
             vid_cap.release()
             break
-        #if somezone over max car
-        # if st.session_state['max_detect'] != 0:
-        #     json_data = json.dumps(json_data)
-        #     for i in range(0, len(count.object_counts)):
-        #         if count.object_counts[i] > st.session_state['max_detect']:
-        #             response = requests.post(st.session_state['url'], data = json_data , headers = {'Content-Type': 'application/json'})
-        # #if interval time
-        # elif st.session_state['interval'] != 0:
-        #     json_data = json.dumps(json_data)
-        #     if time.time() - start_time > st.session_state['interval']:
-        #         # btn = True
-        #         start_time = time.time()
-        #         print(f"Interval {st.session_state['interval']}")
-        #         response = requests.post(st.session_state['url'], data = json_data , headers = {'Content-Type': 'application/json'})
+        # if somezone over max car
+        if st.session_state['max_detect'] != 0:
+            for i in range(0, len(count.object_counts)):
+                if count.object_counts[i] > st.session_state['max_detect']:
+                    response = requests.post(st.session_state['url'], data = jsons , headers = {'Content-Type': 'application/json'})
+        #if interval time
+        elif st.session_state['interval'] != 0:
+            if time.time() - start_time > st.session_state['interval']:
+                # btn = True
+                start_time = time.time()
+                print(f"Interval {st.session_state['interval']}")
+                response = requests.post(st.session_state['url'], data = jsons , headers = {'Content-Type': 'application/json'})
         
 
 
@@ -341,7 +339,8 @@ def detect(source, list_polygon, conf, model):
             break
     if btn:
         json_data = json.dumps(json_data)
-        response = requests.post(st.session_state['url'], data = json_data , headers = {'Content-Type': 'application/json'})
+        print(jsons)
+        response = requests.post(st.session_state['url'], data = jsons , headers = {'Content-Type': 'application/json'})
         print(response) 
         st.write(response.text)
 
